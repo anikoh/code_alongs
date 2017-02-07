a9041bc2a8ca70fa788236254594de2562917b5d
Aniko Hazi <anikoh@gmail.com>
2017-02-07 12:26:52 +1100

    Javascript intro w01 tue

---
+++ 01-javascript/intro/intro.js
@@ -0,0 +1,36 @@
+console.log('introduction to ecmascript');
+
+
+if (5 > 4){
+  console.log('math is so easy');
+}
+
+var fullSlackRule = true;
+
+if (fullSlackRule) {
+  console.log('yay full slack');
+}
+
+//truthy or falsey
+
+var name = '';
+if (name) {
+  console.log('hello' + name);
+}
+
+var points = 0;
+if (points) {
+  console.log('you have' + points + 'points');
+}
+
+var firstname;
+
+if (firstname){
+  console.log('your first name is' + firstname);
+}
+
+// no need to say if (won === true)
+
+// if (won){
+//   console.log('congrats');
+// }
---
+++ 01-javascript/intro/yearcheck.js
@@ -0,0 +1,15 @@
+
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+
+var currentTest = new Date().getFullYear();
+
+if (currentYear > 2015){
+  console.log("Greetings from the future!");
+}
+else if(currentYear < 2015){
+  console.log("Whoa! Blast from the past!");
+}
+else if (currentYear === 2015){
+  console.log("I'm in the present!");
+}
